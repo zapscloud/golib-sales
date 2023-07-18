ed0575f196ee344933010fdf70dbbe2c9c8f8dcb
Karthi <karthikeyan.n@inforios.com>
2023-07-18 11:51:27 +0530

    Updates

--- sales_repository/customer_type_dao.go
+++ sales_repository/customer_type_dao.go
@@ -1,32 +1,33 @@
-package customer_repository
+package sales_repository
 
 import (
 	"github.com/zapscloud/golib-dbutils/db_common"
-	"github.com/zapscloud/golib-sales/sales_repository/mongodb_repository/customer_mongodb_repository"
+	"github.com/zapscloud/golib-sales/sales_repository/mongodb_repository"
+
 	"github.com/zapscloud/golib-utils/utils"
 )
 
 // CustomerTypeDao - Card DAO Repository
 type CustomerTypeDao interface {
 	// InitializeDao
-	InitializeDao(client utils.Map, businessId string, customerId string)
+	InitializeDao(client utils.Map, businessId string)
 	//List - List all Collections
 	List(filter string, sort string, skip int64, limit int64) (utils.Map, error)
 	// Get - Get by code
-	Get(TypeId string) (utils.Map, error)
+	Get(CustomerTypeId string) (utils.Map, error)
 	// Find - Find by filter
 	Find(filter string) (utils.Map, error)
 	// Create - Create Collection
 	Create(indata utils.Map) (utils.Map, error)
 	// Update - Update Collection
-	Update(TypeId string, indata utils.Map) (utils.Map, error)
+	Update(CustomerTypeId string, indata utils.Map) (utils.Map, error)
 	// Delete - Delete Collection
-	Delete(TypeId string) (int64, error)
+	Delete(CustomerTypeId string) (int64, error)
 }
 
-// NewCustomerTypeDao - Contruct Business Type Dao
-func NewCustomerTypeDao(client utils.Map, businessId string, customerId string) CustomerTypeDao {
-	var daoType CustomerTypeDao = nil
+// NewCustomerTypeDao - Contruct Business CustomerType Dao
+func NewCustomerTypeDao(client utils.Map, business_id string) CustomerTypeDao {
+	var daoCustomerType CustomerTypeDao = nil
 
 	// Get DatabaseType and no need to validate error
 	// since the dbType was assigned with correct value after dbService was created
@@ -34,17 +35,17 @@ func NewCustomerTypeDao(client utils.Map, businessId string, customerId string) 
 
 	switch dbType {
 	case db_common.DATABASE_TYPE_MONGODB:
-		daoType = &customer_mongodb_repository.CustomerTypeMongoDBDao{}
+		daoCustomerType = &mongodb_repository.CustomerTypeMongoDBDao{}
 	case db_common.DATABASE_TYPE_ZAPSDB:
 		// *Not Implemented yet*
 	case db_common.DATABASE_TYPE_MYSQLDB:
 		// *Not Implemented yet*
 	}
 
-	if daoType != nil {
+	if daoCustomerType != nil {
 		// Initialize the Dao
-		daoType.InitializeDao(client, businessId, customerId)
+		daoCustomerType.InitializeDao(client, business_id)
 	}
 
-	return daoType
+	return daoCustomerType
 }
--- sales_services/customer_type_service.go
+++ sales_services/customer_type_service.go
@@ -1,4 +1,4 @@
-package customer_services
+package sales_services
 
 import (
 	"fmt"
@@ -10,7 +10,6 @@ import (
 	"github.com/zapscloud/golib-platform/platform_repository"
 	"github.com/zapscloud/golib-sales/sales_common"
 	"github.com/zapscloud/golib-sales/sales_repository"
-	"github.com/zapscloud/golib-sales/sales_repository/customer_repository"
 	"github.com/zapscloud/golib-utils/utils"
 )
 
@@ -18,35 +17,35 @@ type CustomerTypeService interface {
 	// List - List All records
 	List(filter string, sort string, skip int64, limit int64) (utils.Map, error)
 	// Get - Find By Code
-	Get(TypeId string) (utils.Map, error)
+	Get(CustomerTypeId string) (utils.Map, error)
 	// Find - Find the item
 	Find(filter string) (utils.Map, error)
 	// Create - Create Service
 	Create(indata utils.Map) (utils.Map, error)
 	// Update - Update Service
-	Update(TypeId string, indata utils.Map) (utils.Map, error)
+	Update(CustomerTypeId string, indata utils.Map) (utils.Map, error)
 	// Delete - Delete Service
-	Delete(TypeId string, delete_permanent bool) error
+	Delete(CustomerTypeId string, delete_permanent bool) error
+
+	
+	
 
 	EndService()
 }
 
-type customerTypeBaseService struct {
+type CustomerTypeBaseService struct {
 	db_utils.DatabaseService
-	daoCustomerType customer_repository.CustomerTypeDao
-	daoBusiness     platform_repository.BusinessDao
-	daoCustomer     sales_repository.CustomerDao
-
-	child      CustomerTypeService
-	businessId string
-	customerId string
+	daoCustomerType sales_repository.CustomerTypeDao
+	daoBusiness platform_repository.BusinessDao
+	child       CustomerTypeService
+	businessId  string
 }
 
 // NewCustomerTypeService - Construct CustomerType
 func NewCustomerTypeService(props utils.Map) (CustomerTypeService, error) {
 	funcode := sales_common.GetServiceModuleCode() + "M" + "01"
 
-	p := customerTypeBaseService{}
+	p := CustomerTypeBaseService{}
 	err := p.OpenDatabaseService(props)
 	if err != nil {
 		log.Fatal(err)
@@ -58,28 +57,13 @@ func NewCustomerTypeService(props utils.Map) (CustomerTypeService, error) {
 		return p.errorReturn(err)
 	}
 
-	// Verify whether the User id data passed
-	customerId, err := utils.GetMemberDataStr(props, sales_common.FLD_CUSTOMER_ID)
-	if err != nil {
-		return p.errorReturn(err)
-	}
-
 	// Assign the BusinessId
 	p.businessId = businessId
-	p.customerId = customerId
 	p.initializeService()
 
-	// Verify the Business Exists
 	_, err = p.daoBusiness.Get(businessId)
 	if err != nil {
-		err := &utils.AppError{ErrorCode: funcode + "01", ErrorMsg: "Invalid BusinessId", ErrorDetail: "Given BusinessId is not exist"}
-		return p.errorReturn(err)
-	}
-
-	// Verify the Customer Exist
-	_, err = p.daoCustomer.Get(customerId)
-	if err != nil {
-		err := &utils.AppError{ErrorCode: funcode + "01", ErrorMsg: "Invalid CustomerId", ErrorDetail: "Given CustomerId is not exist"}
+		err := &utils.AppError{ErrorCode: funcode + "01", ErrorMsg: "Invalid business_id", ErrorDetail: "Given business_id is not exist"}
 		return p.errorReturn(err)
 	}
 
@@ -88,44 +72,45 @@ func NewCustomerTypeService(props utils.Map) (CustomerTypeService, error) {
 	return &p, err
 }
 
-// EndLoyaltyCardService - Close all the services
-func (p *customerTypeBaseService) EndService() {
+// EndService - Close all the services
+func (p *CustomerTypeBaseService) EndService() {
 	log.Printf("EndService ")
 	p.CloseDatabaseService()
 }
 
-func (p *customerTypeBaseService) initializeService() {
+func (p *CustomerTypeBaseService) initializeService() {
 	log.Printf("CustomerTypeService:: GetBusinessDao ")
-	p.daoCustomerType = customer_repository.NewCustomerTypeDao(p.GetClient(), p.businessId, p.customerId)
+	p.daoCustomerType = sales_repository.NewCustomerTypeDao(p.GetClient(), p.businessId)
 	p.daoBusiness = platform_repository.NewBusinessDao(p.GetClient())
-	p.daoCustomer = sales_repository.NewCustomerDao(p.GetClient(), p.businessId)
 }
 
 // List - List All records
-func (p *customerTypeBaseService) List(filter string, sort string, skip int64, limit int64) (utils.Map, error) {
+func (p *CustomerTypeBaseService) List(filter string, sort string, skip int64, limit int64) (utils.Map, error) {
 
-	log.Println("customerTypeBaseService::FindAll - Begin")
+	log.Println("CustomerTypeBaseService::FindAll - Begin")
 
 	listdata, err := p.daoCustomerType.List(filter, sort, skip, limit)
 	if err != nil {
 		return nil, err
 	}
 
-	log.Println("customerTypeBaseService::FindAll - End ")
+	log.Println("CustomerTypeBaseService::FindAll - End ")
 	return listdata, nil
 }
 
 // Get - Find By Code
-func (p *customerTypeBaseService) Get(TypeId string) (utils.Map, error) {
-	log.Printf("customerTypeBaseService::Get::  Begin %v", TypeId)
+func (p *CustomerTypeBaseService) Get(CustomerTypeId string) (utils.Map, error) {
+	log.Printf("CustomerTypeBaseService::Get::  Begin %v", CustomerTypeId)
 
-	data, err := p.daoCustomerType.Get(TypeId)
-
-	log.Println("customerTypeBaseService::Get:: End ", err)
+	data, err := p.daoCustomerType.Get(CustomerTypeId)
+	if err != nil {
+		return nil, err
+	}
+	log.Println("CustomerTypeBaseService::Get:: End ", err)
 	return data, err
 }
 
-func (p *customerTypeBaseService) Find(filter string) (utils.Map, error) {
+func (p *CustomerTypeBaseService) Find(filter string) (utils.Map, error) {
 	fmt.Println("CustomerTypeService::FindByCode::  Begin ", filter)
 
 	data, err := p.daoCustomerType.Find(filter)
@@ -134,23 +119,24 @@ func (p *customerTypeBaseService) Find(filter string) (utils.Map, error) {
 }
 
 // Create - Create Service
-func (p *customerTypeBaseService) Create(indata utils.Map) (utils.Map, error) {
+func (p *CustomerTypeBaseService) Create(indata utils.Map) (utils.Map, error) {
 
 	log.Println("CustomerTypeService::Create - Begin")
-	var TypeId string
+	var CustomerTypeId string
 
 	dataval, dataok := indata[sales_common.FLD_CUSTOMER_TYPE_ID]
 	if dataok {
-		TypeId = strings.ToLower(dataval.(string))
+		CustomerTypeId = strings.ToLower(dataval.(string))
 	} else {
-		TypeId = utils.GenerateUniqueId("crt")
-		log.Println("Unique CustomerType ID", TypeId)
+		CustomerTypeId = utils.GenerateUniqueId("cust")
+		log.Println("Unique CustomerType ID", CustomerTypeId)
 	}
 
 	// Assign BusinessId
 	indata[sales_common.FLD_BUSINESS_ID] = p.businessId
-	indata[sales_common.FLD_CUSTOMER_TYPE_ID] = TypeId
+	indata[sales_common.FLD_CUSTOMER_TYPE_ID] = CustomerTypeId
 
+	
 	data, err := p.daoCustomerType.Create(indata)
 	if err != nil {
 		return utils.Map{}, err
@@ -161,35 +147,37 @@ func (p *customerTypeBaseService) Create(indata utils.Map) (utils.Map, error) {
 }
 
 // Update - Update Service
-func (p *customerTypeBaseService) Update(TypeId string, indata utils.Map) (utils.Map, error) {
+func (p *CustomerTypeBaseService) Update(CustomerTypeId string, indata utils.Map) (utils.Map, error) {
 
 	log.Println("CustomerTypeService::Update - Begin")
 
-	// Delete Key values
+	// Delete the Key fields if exist
 	delete(indata, sales_common.FLD_BUSINESS_ID)
-	delete(indata, sales_common.FLD_CUSTOMER_ID)
 	delete(indata, sales_common.FLD_CUSTOMER_TYPE_ID)
 
-	data, err := p.daoCustomerType.Update(TypeId, indata)
+	
+	
+
+	data, err := p.daoCustomerType.Update(CustomerTypeId, indata)
 
 	log.Println("CustomerTypeService::Update - End ")
 	return data, err
 }
 
 // Delete - Delete Service
-func (p *customerTypeBaseService) Delete(TypeId string, delete_permanent bool) error {
+func (p *CustomerTypeBaseService) Delete(CustomerTypeId string, delete_permanent bool) error {
 
-	log.Println("CustomerTypeService::Delete - Begin", TypeId)
+	log.Println("CustomerTypeService::Delete - Begin", CustomerTypeId)
 
 	if delete_permanent {
-		result, err := p.daoCustomerType.Delete(TypeId)
+		result, err := p.daoCustomerType.Delete(CustomerTypeId)
 		if err != nil {
 			return err
 		}
 		log.Printf("Delete %v", result)
 	} else {
 		indata := utils.Map{db_common.FLD_IS_DELETED: true}
-		data, err := p.Update(TypeId, indata)
+		data, err := p.Update(CustomerTypeId, indata)
 		if err != nil {
 			return err
 		}
@@ -200,7 +188,7 @@ func (p *customerTypeBaseService) Delete(TypeId string, delete_permanent bool) e
 	return nil
 }
 
-func (p *customerTypeBaseService) errorReturn(err error) (CustomerTypeService, error) {
+func (p *CustomerTypeBaseService) errorReturn(err error) (CustomerTypeService, error) {
 	// Close the Database Connection
 	p.CloseDatabaseService()
 	return nil, err
